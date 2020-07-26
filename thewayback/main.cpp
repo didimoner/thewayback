5f12c22d109279f8c65997c7d5c6530d03e8c557
didimoner <didimoner@gmail.com>
2020-07-26 12:57:59 +0300

    res dir path util

--- thewayback/main.cpp
+++ thewayback/main.cpp
@@ -1,11 +1,16 @@
 #include <iostream>
 #include <SDL.h>
+#include "res_path.h"
+#include <string>
 
 int main(int, char**) {
 	if (SDL_Init(SDL_INIT_VIDEO) != 0) {
 		std::cout << "SDL_Init Error: " << SDL_GetError() << std::endl;
 		return 1;
 	}
+
+	std::cout << "Resource path is: " << getResourcePath() << std::endl;
+
 	SDL_Quit();
 	return 0;
 }
