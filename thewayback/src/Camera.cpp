d7d7da7872db8f7dd6c1e4921ea589433fcb36aa
didimoner <didimoner@gmail.com>
2020-09-12 18:21:22 +0300

    fir for player initial position

--- thewayback/src/Camera.cpp
+++ thewayback/src/Camera.cpp
@@ -11,18 +11,5 @@ void Camera::setPosition(float x, float y) {
 }
 
 void Camera::update() {
-	InputHandler* pInputHandler = InputHandler::instance();
-	if (pInputHandler->isKeyPressed(SDL_SCANCODE_UP)) {
-		m_velocity.setY(-1.f);
-	} else if (pInputHandler->isKeyPressed(SDL_SCANCODE_DOWN)) {
-		m_velocity.setY(1.f);
-	} else if (pInputHandler->isKeyPressed(SDL_SCANCODE_LEFT)) {
-		m_velocity.setX(-1.f);
-	} else if (pInputHandler->isKeyPressed(SDL_SCANCODE_RIGHT)) {
-		m_velocity.setX(1.f);
-	} else {
-		m_velocity.set(0, 0);
-	}
-
 	m_position += m_velocity;
 }
--- thewayback/src/SDLGameObject.cpp
+++ thewayback/src/SDLGameObject.cpp
@@ -2,6 +2,7 @@
 #include "SDLGameObject.h"
 #include "TextureManager.h"
 #include "Game.h"
+#include "GameState.h"
 #include "GameObject.h"
 
 void SDLGameObject::init(float x, float y, int w, int h, std::string textureId) {
@@ -17,9 +18,10 @@ void SDLGameObject::update() {
 }
 
 void SDLGameObject::draw() {
+	Vector2f cameraPos = Game::instance()->getCurrentState()->getCameraPosition();
 	TextureManager::instance()->drawFrame(m_textureId, 
-		m_position.getX(), m_position.getY(), m_width, m_height, 
-		m_currentRow, m_currentFrame, Game::instance()->getRenderer());
+		m_position.getX() - cameraPos.getX(), m_position.getY() - cameraPos.getY(),
+		m_width, m_height, m_currentRow, m_currentFrame, Game::instance()->getRenderer());
 }
 
 void SDLGameObject::clean() {
